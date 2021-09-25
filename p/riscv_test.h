fa40573de1c03edd1d048682471109eb73f91664
liweiwei <liweiwei@iscas.ac.cn>
2021-09-25 08:32:07 +0800

    revert the align of tohost/fromhost to 64 bytes

--- p/riscv_test.h
+++ p/riscv_test.h
@@ -260,8 +260,8 @@ reset_vector:                                                           \
 #define RVTEST_DATA_BEGIN                                               \
         EXTRA_DATA                                                      \
         .pushsection .tohost,"aw",@progbits;                            \
-        .align 8; .global tohost; tohost: .dword 0; .size tohost, 8;    \
-        .align 8; .global fromhost; fromhost: .dword 0; .size fromhost, 8;\
+        .align 6; .global tohost; tohost: .dword 0; .size tohost, 8;    \
+        .align 6; .global fromhost; fromhost: .dword 0; .size fromhost, 8;\
         .popsection;                                                    \
         .align 4; .global begin_signature; begin_signature:
 
